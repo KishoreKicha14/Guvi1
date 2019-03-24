c36595c880db9f920211063b8329cc7905ad95bf
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-03-24 22:56:46 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,18 @@
+n=input().split()
+list=[]
+for i in n:
+   list.append(i)
+a=list[0]
+b=list[1]
+l=len(a)
+l1=len(b)
+le=min(l,l1)
+
+dif=l1-l
+
+for i in range(0,le):
+     
+   if(a[i]!=b[i]):
+      dif=dif+1
+      
+print(dif)
