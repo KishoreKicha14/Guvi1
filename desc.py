3857bca70e4944d75063a0feeade654a5a9e8d2e
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-03-26 19:32:16 +0530

    Update desc.py

--- desc.py
+++ desc.py
@@ -1,8 +1,19 @@
 m=int(input())
-n=input().split()
-list=[]
+list=input().split()
+l=[]
 for i in range(0,m):
-   list.append(int(n[i]))
+   list.insert(i,int(list[i]))
+   list.remove(list[i+1])
+   
 list.sort(reverse=True)
-for i in list:
-   print(i,end="")
+for i in range(0,m):
+   l.append(str(list[i]))
+
+l=''.join(l)
+
+while l[0] == "0":
+    if(len(l)==1):
+      break
+    l = l[1:]
+
+print(l)
