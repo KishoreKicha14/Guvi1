4fdc6dcb9db29aed0cbd6b27d5b31f0ed40f0594
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-03-26 19:53:18 +0530

    Create unique.py

---
+++ unique.py
@@ -0,0 +1,22 @@
+l= int(input())
+arr = input()
+n = list(map(int,arr.split(' '))) 
+n.sort()
+d=[]
+counter=1
+f=0
+
+for i in range(0,len(n)-1):
+   
+   if(n[i]==n[i+1]):
+      if(f==0):         
+         f=1
+         d.append(n[i])
+   else:
+      
+      f=0
+
+for i in d:
+   n.remove(i)
+   n.remove(i)
+print(n[0])
