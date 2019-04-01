512ebe8638a1047396494ddc1b3552baf0661b91
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-04-01 17:05:17 +0530

    Update firstduplicate.py

--- firstduplicate.py
+++ firstduplicate.py
@@ -4,11 +4,12 @@ def printRepeating(arr, size):
         if arr[abs(arr[i])] >= 0: 
             arr[abs(arr[i])] = -arr[abs(arr[i])] 
         else: 
-            print (abs(arr[i]))
-            break
+            lis.append(abs(arr[i]))
 m=int(input())
 n=input().split()
+lis=[]
 for i in range(0,len(n)):
    n.insert(i,int(n[i]))
    n.remove(n[i+1])
 printRepeating(n,m-1)
+print(lis[0])
