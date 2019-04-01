1e3d2e10dad6d952dc5a3c3577f6db789787ff3f
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-04-01 19:48:02 +0530

    Create comandso.py

---
+++ comandso.py
@@ -0,0 +1,13 @@
+n=int(input())
+m=input().split()
+for i in range(0,len(m)):
+      m.insert(i,int(m[i]))
+      m.remove(m[i+1])
+for i in range(n-1):
+   r=input().split()
+   for i in range(0,len(r)):
+      m.append(int(r[i]))
+   m.sort()
+
+
+print(m)
