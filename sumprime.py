d00e9208479e5cd8c13112bfb767aa6b0a9851bc
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-04-09 22:37:43 +0530

    Create sumprime.py

---
+++ sumprime.py
@@ -0,0 +1,18 @@
+def prime(n):
+   if n < 1:
+      return(False)
+   else:
+      for i in range(2,n):
+         if (n % i) == 0:
+            return(False)
+            break
+         else:
+            return(True)
+
+c=0
+n=[int(s) for s in input().split()]
+for i in(n[0],n[1]):
+   for j in range(i+1,n[1]):
+      if(prime(i+j)):
+         c=c+1
+print(c)
