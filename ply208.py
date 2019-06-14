34240d3d31878c72cf5344b97e86f14be80c1418
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-06-14 11:54:46 +0100

    Update and rename ply209.py to ply208.py

--- ply208.py
+++ ply208.py
@@ -2,13 +2,13 @@ n=int(input())
 r=[]
 for i in range(n):
     r.append([int(s) for s in input().split()])
-l=1
-re=1
+l=0
+re=0
 le=n-1
 st=0
 for i in range(n):
-   l=l*r[i][st]
+   l=l+r[i][st]
    st=st+1
-   re=re*r[i][le]
+   re=re+r[i][le]
    le=le-1
-print(l+re)
+print(l*re)
