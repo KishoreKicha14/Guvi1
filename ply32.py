7b1ff6db934828364622d9353b036814eb2e308e
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-06-14 10:23:14 +0100

    Update and rename ply31.py to ply32.py

---
+++ ply32.py
@@ -0,0 +1,17 @@
+m=[int(s) for s in input().split()]
+n=[int(s) for s in input().split()]
+f=1
+for i in m:
+    if(f==1):
+        for j in n:
+            r=0
+            if(i==j):
+                r=1
+                break
+        if(r==0):
+             f=0
+if(f==0):
+    print("no")
+else:
+    print("yes")
+
