5c1eacd0684b9d90c5699c364efcfb76b4d2388a
KishoreKicha14 <30365405+KishoreKicha14@users.noreply.github.com>
2019-06-14 12:12:10 +0100

    Create cat.py

---
+++ cat.py
@@ -0,0 +1,10 @@
+def catalan(n):
+    if n<=1:
+        return 1
+    res =0
+    for i in range(n):
+        res+=catalan(i)*catalan(n-i-1)
+        print(res,i)
+    return res
+for i in range(4):
+    print(catalan(i))
