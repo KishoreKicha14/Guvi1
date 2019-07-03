50cc6df1d4e85fa96da3a075ae36822cc0608f38
Kishore Kumaar Natarajan <30365405+KishoreKicha14@users.noreply.github.com>
2019-07-03 13:51:39 +0530

    Update and rename p2_5.py to p2_5E.py

--- p2_5E.py
+++ p2_5E.py
@@ -1,14 +1,15 @@
 b =[int(s) for s in input().split()]
-ml=1
+m=1
 l=1
 for i in range(1,len(b)):
     
     if(b[i]>b[i-1]):
         l=l+1
     else:
-        if(l>ml):
-            ml=l
+        if(l>m):
+            m=l
             l=1
-if(l>ml):
-    ml=l
-print(ml)
+if(l>m):
+    m=l
+print(m)
+          
