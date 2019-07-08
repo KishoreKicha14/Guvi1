1f76da720b4e130b8c9da0a12d3e7673df31fc24
Kishore Kumaar Natarajan <30365405+KishoreKicha14@users.noreply.github.com>
2019-07-08 22:47:55 +0530

    Update b9_6E.py

--- b9_6E.py
+++ b9_6E.py
@@ -3,10 +3,10 @@ r=[]
 f=1
 for i in n:
     if(i in r):
-        print("no")
+        print("No")
         f=0
         break
     else:
         r.append(i)
 if(f==1):
-    print("yes")
+    print("Yes")
