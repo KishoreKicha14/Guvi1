93b1c52e5ea6390b26d0f33338f0cf77dedeab45
Kishore Kumaar Natarajan <30365405+KishoreKicha14@users.noreply.github.com>
2019-07-09 08:24:28 +0530

    Update h10_3.py

--- h10_3.py
+++ h10_3.py
@@ -1,11 +1,13 @@
 n=input().split()
+c=0
 m=[]
 for i in n:
     r=[]
     for j in range(len(i)):
-        if(j%2==0):
+        if(c%2==0):
             r.append(i[j].capitalize())
         else:
             r.append(i[j])
+        c=c+1
     m.append("".join(r))
 print(" ".join(m))  
