bb985bab4ed2e5031a889164b4d15cf6cb1f0d20
Kishore Kumaar Natarajan <30365405+KishoreKicha14@users.noreply.github.com>
2019-07-15 03:47:14 -0700

    Update pro49.py

--- pro49.py
+++ pro49.py
@@ -2,15 +2,25 @@ n=input()
 m=input()
 ln=len(n)
 lm=len(m)
-l=ln
+if(ln<=lm):
+    a=n
+    b=m
+else:
+    b=n
+    a=m
+ml=min(ln,lm)
+ml1=max(ln,lm)
+l=ml
 c=0
 p=0
-for i in range(1,len(n)):
-    if n[i]==n[0]:
+for i in range(1,len(a)):
+    if a[i]==a[0]:
         l=i
         break
-c=ln//l
+c=ml//l
 for i in range(1,c+1):
-    if lm%(i*l)==0:
+   
+    if ml1%(i*l)==0:
         p=p+1
 print(p)
+
