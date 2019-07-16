26137f67b0b38720e542a9e54edc98ec87690984
Kishore Kumaar Natarajan <30365405+KishoreKicha14@users.noreply.github.com>
2019-07-16 01:54:39 -0700

    Create hun29.py

---
+++ hun29.py
@@ -0,0 +1,12 @@
+def kadane(A):
+    c=A[0]
+    g=A[0]
+    for i in range(1,len(A)):
+        c=max(A[i],c+A[i])
+        if c>g:
+            g=c
+    return g
+m=int(input())
+n=[int(s) for s in input().split()]
+k=kadane(n)
+print(k)
